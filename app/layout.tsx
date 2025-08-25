25de1e4e3357c3bc8755982bcb8cc8f721105b77
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2025-08-25 16:41:14 -0400

    header alignment fix and shadcn/ui components and button

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={` ${fontSans.className} antialiased`}
+        className={` ${fontSans.className} ${fontSerif.variable} antialiased`}
       >
         <Header />
         {children}
--- components/common/header.tsx
+++ components/common/header.tsx
@@ -19,25 +19,25 @@ export default function Header() {
     ]
 
     const navLinkElements = navLinks.map( ({href, label }) => (
-        <Link 
-            href={href} 
-            key={label}
-            className="text-sm font-medium hover:underline underline-offset-4"
-        >
-            {label}
-        </Link>
-    ))
+                        <Link 
+                            href={href} 
+                            key={label}
+                            className="text-sm font-medium hover:underline underline-offset-4"
+                        >
+                            {label}
+                        </Link>
+                    ))
 
     return (
         <header className="px-4 lg:px-6 h-14 flex items-center">
-            <div className="w-full mx-auto flex items-center justify-content container px-4">
+            <div className="w-full mx-auto flex items-center justify-between container px-4">
                 <Link 
                     href="/" 
                     className="flex items-center justify-center"
                 >
                     <h1 className="font-serif">Merito</h1>
                 </Link>
-                <nav className="flex items-center gap-4">
+                <nav className="flex items-center gap-4 sm:gap-6">
                     {navLinkElements}
                     { isSignedIn ? ( <></>) : (
                         <Link 
