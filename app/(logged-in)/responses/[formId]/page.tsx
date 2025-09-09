8e39995030117e164d4e6afe1df3adb198c4c0ab
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2025-09-09 19:03:38 -0400

    did npm run build

--- app/(logged-in)/responses/[formId]/page.tsx
+++ app/(logged-in)/responses/[formId]/page.tsx
@@ -1,11 +1,11 @@
 import TestimonialsList from "@/components/reviews/testimonials-list"
-import { Button } from "@/components/ui/button";
 import { getReviewFormById, getTestimonialsByFormId } from "@/lib/reviews";
 import { Form } from "@prisma/client"
-import Link from "next/link"
 import { notFound } from "next/navigation"
 
-type Params = Promise<{ formId: Form["id"] }>
+type Params = Promise<{ 
+  formId: Form["id"]
+}>
 
 export default async function ResponsesPage({ params }: { params: Params }) {
 
