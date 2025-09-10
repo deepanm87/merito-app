a4adb079374a3ae47dc7318df689cbd2c4cde25f
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2025-09-10 15:17:59 -0400

    bug fixes

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,17 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
 
 const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
   "/",
   "/reviews/(.*)/embed",
-]);
+])
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
-    await auth.protect();
+    await auth.protect()
   }
-});
+})
 
 export const config = {
   matcher: [
@@ -20,4 +20,6 @@ export const config = {
     // Always run for API routes
     "/(api|trpc)(.*)",
   ],
-};
+}
+
+
